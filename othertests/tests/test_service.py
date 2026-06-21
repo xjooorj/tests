aff771f9d78ffe86bac592487f0d19ad552f362c
xjooorj <notyourjorj@gmail.com>
2026-06-21 15:27:59 +0500

    mocking 2

--- othertests/tests/test_service.py
+++ othertests/tests/test_service.py
@@ -1,13 +1,24 @@
-import pytest
-import othertests as service
+import othertests.service as service
 import unittest.mock as mock
+import requests
 
 @mock.patch("othertests.service.get_user_from_db")
 def test_get_user_from_db(mock_get_user_from_db):
-    mock_get_user_from_db.return_value = "mocked oybek"
-    user_name = service.get_user_from_db[1]
+    mock_get_user_from_db.return_value = "mocked hilol"
+    user_name = service.get_user_from_db(2)
+    assert user_name == "mocked hilol"
+    
 
-    assert user_name == "mocked oybek"
+
+@mock.patch("requests.get")
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"id": 1, "name": "John Doe"}
+    mock_get.return_value = mock_response
+    data = service.get_users()
+    assert data == {"id": 1, "name": "John Doe"}
 
 
-    pass
+
+
