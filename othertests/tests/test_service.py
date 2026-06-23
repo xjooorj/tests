2be1347ac2b50275446c60034fe0a2e5ee058916
xjooorj <notyourjorj@gmail.com>
2026-06-23 11:48:00 +0500

    mocking 3

--- othertests/tests/test_service.py
+++ othertests/tests/test_service.py
@@ -1,3 +1,4 @@
+import pytest
 import othertests.service as service
 import unittest.mock as mock
 import requests
@@ -18,6 +19,15 @@ def test_get_users(mock_get):
     mock_get.return_value = mock_response
     data = service.get_users()
     assert data == {"id": 1, "name": "John Doe"}
+
+
+@mock.patch("requests.get")
+def test_get_users_error(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 400
+    mock_get.return_value - mock_response
+    with pytest.raises(requests.HTTPError):
+        service.get_users()
 
 
 
